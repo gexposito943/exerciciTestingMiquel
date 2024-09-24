54177bc20e5f15c924fb99ebc2d7f55e46bef3a9
gexposito943 <gexposito943@boscdelacoma.cat>
2024-09-24 20:16:29 +0200

    commits de prova

--- src/app/formulari/formulari.component.spec.ts
+++ src/app/formulari/formulari.component.spec.ts
@@ -1,23 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FormulariComponent } from './formulari.component';
 
 describe('FormulariComponent', () => {
-  let component: FormulariComponent;
-  let fixture: ComponentFixture<FormulariComponent>;
+    let component: FormulariComponent;
+    let fixture: ComponentFixture<FormulariComponent>;
+    let compiled: HTMLElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FormulariComponent]
-    })
-    .compileComponents();
+  
 
-    fixture = TestBed.createComponent(FormulariComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [FormulariComponent]
+        })
+        
+        .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(FormulariComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        compiled=fixture.nativeElement as HTMLElement;
+
+    });
+
+    it('Hauriem de tenir 5 inputs', () => {
+        const inputs:HTMLInputElement|null= querySelectorAll('input');
+        expect(inputs.length).toBe(5);
+    });
+
+    it('Hauriem de tenir 5 labels', () => {
+        const labels = fixture.HTML.querySelectorAll('label');
+        expect(labels.length).toBe(5);
+    });
+
 });
