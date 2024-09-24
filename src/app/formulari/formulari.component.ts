b688cddd30b44802c4380ffdfc8e50d693528071
Jonathan <jrodas753@boscdelacoma.cat>
2024-09-24 19:49:55 +0200

    creat component formulari

---
+++ src/app/formulari/formulari.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulari',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulari.component.html',
+  styleUrl: './formulari.component.css'
+})
+export class FormulariComponent {
+
+}
