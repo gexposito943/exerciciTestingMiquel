f458bab0aa68d7439db4bb2d135399aee1b06389
Jonathan <jrodas753@boscdelacoma.cat>
2024-09-25 17:10:45 +0200

    funcionant ng serve

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+import { FormulariComponent } from './formulari/formulari.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent,  FormulariComponent]
+
     }).compileComponents();
   });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormulariComponent } from "./formulari/formulari.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormulariComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
